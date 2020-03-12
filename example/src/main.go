e060b152a3f5c0e29a8f2bdd677275c4c50da608
54meteor1 <meteorrefresh@gmail.com>
2020-03-12 23:15:57 +0800

    no message

--- example/src/main.go
+++ example/src/main.go
@@ -1,32 +1,47 @@
 package main
 import (
+	"container/list"
 	"fmt"
-	"strings"
 )
 func main() {
-	result := StringToLower("AbcdefGHijklMNOPqrstUVWxyz", processCase)
-	fmt.Println(result)
-	result = StringToLower2("AbcdefGHijklMNOPqrstUVWxyz", processCase)
-	fmt.Println(result)
+	copyList()
 }
-//处理字符串，奇数偶数依次显示为大小写
-func processCase(str string) string {
-	result := ""
-	for i, value := range str {
-		if i%2 == 0 {
-			result += strings.ToUpper(string(value))
-		} else {
-			result += strings.ToLower(string(value))
-		}
+//list是值类型，不过采用New()方法声明的是一个指针。所以在拷贝操作和参数传递时具有引用类型的特征。
+func copyList() {
+	//声明list1
+	list1 := list.New()
+	printListInfo2("刚声明的list1：", list1)
+	//给list1赋值
+	list1.PushBack("one")
+	list1.PushBack(2)
+	list1.PushBack("three")
+	list1.PushFront("first")
+	printListInfo2("赋值后的list1", list1)
+	iterateList2(list1)
+	//将list1拷贝给list2。其实拷贝的是地址
+	list2 := list1
+	printListInfo2("刚拷贝的list2", list2)
+	iterateList2(list2)
+	//list2修改后
+	list2.PushBack(250)
+	list2.PushBack(350)
+	list2.PushBack(450)
+	printListInfo2("修改后的list2", list2)
+	iterateList2(list2)
+	//list2的修改是否影响到list1？
+	printListInfo2("修改list2的list1", list1)
+	iterateList2(list1)
+}
+func printListInfo2(info string, l *list.List) {
+	fmt.Println(info + "----------")
+	fmt.Printf("%T:%v \t ， 长度为：%d \n", l, l, l.Len())
+	fmt.Println("----------")
+}
+func iterateList2(l *list.List) {
+	i := 0
+	for e := l.Front(); e != nil; e = e.Next() {
+		i++
+		fmt.Printf("%d:%v \t", i, e.Value)
 	}
-	return result
-}
-func StringToLower(str string, f func(string) string) string {
-	fmt.Printf("%T \n", f)
-	return f(str)
-}
-type caseFunc func(string) string // 声明了一个函数类型。通过type关键字，caseFunc会形成一种新的类型。
-func StringToLower2(str string, f caseFunc) string {
-	fmt.Printf("%T \n", f)
-	return f(str)
+	fmt.Println("\n----------")
 }
